82131ef4efcfc22ea314165438d7421da029072d
GzmBU <gizembusra6@gmail.com>
2023-07-09 22:26:33 +0300

    add commit

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+#include <locale.h>
+#include <string.h>
+#define MAX_SIZE 100 
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    int i, j, lenght;
+    char string[MAX_SIZE];
+    char reverse[MAX_SIZE];
+    printf("Metni Gir: ");
+    gets(string);
+    lenght = strlen(string);
+    j = 0;
+    for (i = lenght - 1; i >= 0; i--)
+    {
+        reverse[j] = string[i];
+        j++;
+    }
+    reverse[j] = '\0';
+    printf("Ters : %s", reverse);
+
+    return  0;
+
+}  
---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+int main()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+
+    printf("Bir tamsayı girin: ");
+    scanf_s("%d", &n);
+
+    originalInteger = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversedInteger = reversedInteger * 10 + remainder;
+        n /= 10;
+    }
+
+    // orignalInteger ve reversedInteger eşitse Palindrom'dur
+    if (originalInteger == reversedInteger)
+        printf("%d Palindrom'dur.", originalInteger);
+    else
+        printf("%d Palindrom değildir.", originalInteger);
+
+    return 0;
+}
